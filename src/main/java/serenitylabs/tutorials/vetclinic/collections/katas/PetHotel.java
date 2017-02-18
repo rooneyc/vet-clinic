617699875f257a039ea3d203dc53572efaf11af3
Conor Rooney <conor.p.rooney@aib.ie>
2017-02-18 15:16:11 +0000

    1st failing test

---
+++ src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
@@ -0,0 +1,15 @@
+package serenitylabs.tutorials.vetclinic.collections.katas;
+
+import serenitylabs.tutorials.vetclinic.Pet;
+
+import java.util.Collection;
+
+/**
+ * Created by 83018 on 18/02/2017.
+ */
+public class PetHotel {
+
+    public Collection<Pet> getPets() {
+        return null;
+    }
+}
