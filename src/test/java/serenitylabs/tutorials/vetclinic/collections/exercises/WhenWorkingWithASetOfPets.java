24f92eb53b2c0a6ced287cf6681ddebc0e19f8fb
Conor Rooney <conor.p.rooney@aib.ie>
2017-02-26 20:38:26 +0000

    WhenWorkingWithAListOfPetNames

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithASetOfPets.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithASetOfPets.java
@@ -5,33 +5,49 @@ import org.junit.Test;
 
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class WhenWorkingWithASetOfPets {
 
     @Test
     public void should_add_Fido_to_the_set_of_pets() {
+
+       //Given
         Set<String> names = Sets.newHashSet();
 
-        // TODO
+        //When
+        names.add("Fido");
+
+        //Then
+        assertThat(names).contains("Fido");
     }
 
     @Test
     public void a_set_of_pets_should_not_contain_duplicates() {
+
+        //Given
         Set<String> names = Sets.newHashSet();
 
+        //When
         names.add("Fido");
         names.add("Felix");
         names.add("Fido");
 
-        // TODO
+        //Then
+        assertThat(names).containsOnlyOnce("Fido");
     }
 
     @Test
     public void adding_several_pets() {
+
+        //Given
         Set<String> names = Sets.newHashSet("Fido","Felix");
 
+        //When
         names.addAll(Sets.newHashSet("Felix","Spot"));
 
-        // TODO
+        //Then
+        assertThat(names).contains("Fido", "Spot", "Felix");
     }
 
 
