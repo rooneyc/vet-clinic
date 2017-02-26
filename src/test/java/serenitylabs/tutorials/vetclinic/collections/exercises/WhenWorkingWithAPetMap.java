a3259ec8f25d965d51c3b0a1cd02bb77201555a2
Conor Rooney <conor.p.rooney@aib.ie>
2017-02-26 21:02:12 +0000

    WhenWorkingWithAPetMap

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAPetMap.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAPetMap.java
@@ -6,77 +6,98 @@ import serenitylabs.tutorials.vetclinic.Pet;
 
 import java.util.*;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class WhenWorkingWithAPetMap {
 
     @Test
     public void pets_can_be_found_by_their_name() {
-        Map<String, Pet> pets = new HashMap<>();
 
+        //Given
+        Map<String, Pet> pets = new HashMap<>();
         Pet fido = Pet.dog().named("Fido");
 
+        //When
         pets.put("Fido", fido);
 
-        // TODO
+        //Then
+        org.assertj.core.api.Assertions.assertThat(pets.get("Fido")).isSameAs(fido);
     }
 
     @Test
     public void should_be_able_to_get_a_default_value_if_no_matching_key_is_present() {
+
+        //Given
         Map<String, Pet> pets = new HashMap<>();
 
         Pet fido = Pet.dog().named("Fido");
         Pet stray = Pet.dog().named("Stray");
 
+        //When
         pets.put("Fido", fido);
         pets.put("Stray", stray);
 
-        // TODO
+        //Then
+        org.assertj.core.api.Assertions.assertThat(pets.getOrDefault("Felix", stray)).isSameAs(stray);
     }
 
     @Test
     public void the_map_keys_should_be_the_pet_names() {
+
+        //Given
         Map<String, Pet> pets = new HashMap<>();
 
         Pet fido = Pet.dog().named("Fido");
         Pet felix = Pet.cat().named("Felix");
 
+        //When
         pets.put("Fido", fido);
         pets.put("Felix", felix);
 
-        // TODO
+        //Then
+        for (Map.Entry<String, Pet> entry: pets.entrySet()) {
+            org.assertj.core.api.Assertions.assertThat(entry.getKey()).isSameAs(entry.getValue().getName());
+        }
     }
 
     @Test
     public void the_map_should_store_pets_in_alphabetical_order() {
-        // TODO: Instantiate the correct type of Map
-        NavigableMap<String, Pet> pets = null;
 
+        //Given
+        NavigableMap<String, Pet> pets = new TreeMap<>();
+
+        //When
         pets.put("Rover", Pet.dog().named("Rover"));
         pets.put("Felix", Pet.cat().named("Felix"));
         pets.put("Spot", Pet.cat().named("Spot"));
 
+        //Then
         assertThat(pets.keySet(), contains("Felix","Rover","Spot"));
     }
 
     @Test
     public void the_map_should_store_pets_in_the_order_they_where_added() {
-        // TODO: Instantiate the correct type of Map
-        Map<String, Pet> pets =  null;
 
+        //Given
+        Map<String, Pet> pets =  new LinkedHashMap<>();
+
+        //When
         pets.put("Spot", Pet.cat().named("Spot"));
         pets.put("Rover", Pet.dog().named("Rover"));
         pets.put("Felix", Pet.cat().named("Felix"));
         pets.put("Fido", Pet.cat().named("Fido"));
 
+        //Then
         assertThat(pets.keySet(), contains("Spot", "Rover","Felix", "Fido"));
     }
 
     @Test
     public void the_map_should_store_pet_leaders_by_breed() {
-        // TODO: Create an EnumMap to define a pet leader for each breed
-        EnumMap<Breed, Pet> petLeaders =  null;
+
+        //Given
+        EnumMap<Breed, Pet> petLeaders =  new EnumMap<>(Breed.class);
 
         petLeaders.put(Breed.Cat, Pet.cat().named("Felix"));
         petLeaders.put(Breed.Dog, Pet.dog().named("Lassie"));
