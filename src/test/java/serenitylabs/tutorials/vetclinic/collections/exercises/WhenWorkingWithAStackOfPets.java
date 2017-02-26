7dd22c742c43e5472976a58b670aff9e9e8ab661
Conor Rooney <conor.p.rooney@aib.ie>
2017-02-26 21:21:11 +0000

    WhenWorkingWithAStackOfPets

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAStackOfPets.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAStackOfPets.java
@@ -14,14 +14,16 @@ public class WhenWorkingWithAStackOfPets {
     @Test
     public void should_store_pets_in_a_stack() {
 
+        //Given
         Deque<Pet> pets = new LinkedList<>();
 
         pets.push(Pet.cat().named("Felix"));
         pets.push(Pet.dog().named("Fido"));
 
-        // TODO: Retrieve the last pet put on the list
-        Pet lastPet = null;
+        //When
+        Pet lastPet = pets.pop();
 
+        //Then
         assertThat(lastPet.getName(),equalTo("Fido"));
         assertThat(pets.size(), equalTo(1));
     }
@@ -29,14 +31,16 @@ public class WhenWorkingWithAStackOfPets {
     @Test
     public void should_see_the_next_item_in_a_stack() {
 
+        //Given
         Deque<Pet> pets = new LinkedList<>();
 
         pets.push(Pet.cat().named("Felix"));
         pets.push(Pet.dog().named("Fido"));
 
-        // TODO: Retrieve the last pet put on the list
-        Pet lastPet = null;
+        //When
+        Pet lastPet = pets.peek();
 
+        //Then
         assertThat(lastPet.getName(),equalTo("Fido"));
         assertThat(pets.size(), equalTo(2));
     }
