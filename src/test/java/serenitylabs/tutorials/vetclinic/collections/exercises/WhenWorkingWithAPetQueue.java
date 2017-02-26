ffb3400fae39e13d4897d0793b51cfaae886cfc7
Conor Rooney <conor.p.rooney@aib.ie>
2017-02-26 21:18:08 +0000

    WhenWorkingWithAPetQueue

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAPetQueue.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAPetQueue.java
@@ -6,27 +6,39 @@ import serenitylabs.tutorials.vetclinic.Pet;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class WhenWorkingWithAPetQueue {
 
     @Test
     public void should_add_Fido_to_the_end_of_the_queue() {
+
+        //Given
         Queue<Pet> waitingList = new LinkedList<>();
 
+        //When
         waitingList.add(Pet.cat().named("Felix"));
         waitingList.add(Pet.dog().named("Fido"));
 
-        // TODO
+        //Then
+        assertThat(waitingList.poll().getName()).isSameAs("Felix");
     }
 
     @Test
     public void should_see_who_is_at_the_top_of_the_queue_without_removing_it() {
+
+        //Given
         Queue<Pet> waitingList = new LinkedList<>();
 
+        //When
         waitingList.add(Pet.cat().named("Felix"));
         waitingList.add(Pet.dog().named("Fido"));
 
-        // TODO
+        //Then
+        assertThat(waitingList.peek().getName()).isSameAs("Felix");
+        assertThat(waitingList.peek().getName()).isSameAs("Felix");
     }
 
 
 }
+
