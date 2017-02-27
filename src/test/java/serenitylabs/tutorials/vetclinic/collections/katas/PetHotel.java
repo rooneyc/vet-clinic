a9e65000bc5866f417517a246d427c69b5b1c6f1
Conor Rooney <conor.p.rooney@aib.ie>
2017-02-27 09:06:39 +0000

    refactoring the pet list field to use a TreeMap with a Java 8 comparator. Assume it should be TreeSet?

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
@@ -3,19 +3,21 @@ package serenitylabs.tutorials.vetclinic.collections.katas;
 import serenitylabs.tutorials.vetclinic.Pet;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.TreeSet;
+
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.naturalOrder;
 
 class PetHotel {
 
-    private Map<String, Pet> pets = new TreeMap<>();
+    private Collection<Pet> pets = new TreeSet<>(comparing(Pet::getName, naturalOrder()));
 
     Collection<Pet> getPets() {
-        return pets.values();
+        return pets;
     }
 
     BookingResponse checkIn(Pet pet) {
-        pets.put(pet.getName(), pet);
+        pets.add(pet);
         return new BookingResponse(pet);
     }
 
