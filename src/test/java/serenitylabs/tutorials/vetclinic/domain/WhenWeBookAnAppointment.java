3188281410b13558fea0def4cbfe49a87b365a07
rooneyc <conor.p.rooney@aib.ie>
2017-04-16 17:09:46 +0100

    Step 5 - Updating the WhenWeBookAnAppointment test

--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeBookAnAppointment.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeBookAnAppointment.java
@@ -5,25 +5,58 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 public class WhenWeBookAnAppointment {
 
     private static final LocalDateTime TODAY_AT_2_PM = LocalDateTime.now().withHour(2).withMinute(0);
+    private static final LocalDateTime TODAY_AT_3_PM = LocalDateTime.now().withHour(3).withMinute(0);
 
     @Test
     public void an_appointment_has_a_patient_name_an_owner_and_a_date() {
+
+        //When
         Appointment appointment = Appointment.forPetCalled("Fido").ownedBy("Fred").at(TODAY_AT_2_PM);
 
-        Assert.assertEquals("Fido",appointment.getPetName());
-        Assert.assertEquals("Fred",appointment.getOwner());
-        Assert.assertEquals(TODAY_AT_2_PM,appointment.getAppointmentTime());
+        //Then
+        Assert.assertEquals("Fido", appointment.getPetName());
+        org.junit.Assert.assertThat(appointment.getPetName(), is(equalTo("Fido")));
+        org.assertj.core.api.Assertions.assertThat(appointment.getPetName()).isEqualTo("Fido");
+
+        Assert.assertEquals("Fred", appointment.getOwner());
+        org.junit.Assert.assertThat(appointment.getOwner(), is(equalTo("Fred")));
+        org.assertj.core.api.Assertions.assertThat(appointment.getOwner()).isEqualTo("Fred");
+
+        Assert.assertEquals(TODAY_AT_2_PM, appointment.getAppointmentTime());
+        org.junit.Assert.assertThat(appointment.getAppointmentTime(), is(equalTo(TODAY_AT_2_PM)));
+        org.assertj.core.api.Assertions.assertThat(appointment.getAppointmentTime()).isEqualTo(TODAY_AT_2_PM);
     }
 
     @Test
     public void an_appointment_can_have_an_optional_reason() {
+
+        //When
         Appointment appointment = Appointment.forPetCalled("Fido").ownedBy("Fred").because("He is sick").at(TODAY_AT_2_PM);
 
-        Assert.assertEquals(true,appointment.getReason().isPresent());
-        Assert.assertEquals("He is sick",appointment.getReason().get());
+        //Then
+        Assert.assertEquals(true, appointment.getReason().isPresent());
+        org.junit.Assert.assertTrue(appointment.getReason().isPresent());
+        org.assertj.core.api.Assertions.assertThat(appointment.getReason().isPresent()).isTrue();
+
+        Assert.assertEquals("He is sick", appointment.getReason().get());
+        org.junit.Assert.assertThat(appointment.getReason().get(), is(equalTo("He is sick")));
+        org.assertj.core.api.Assertions.assertThat(appointment.getReason().get()).isEqualTo("He is sick");
     }
 
+    @Test
+    public void should_be_able_to_check_if_an_appointment_is_before_a_time() throws Exception {
+
+        //When
+        Appointment appointment = Appointment.forPetCalled("Fido").ownedBy("Fred").because("He is sick").at(TODAY_AT_2_PM);
+
+        //Then
+        //org.junit.Assert.assertThat(appointment.isBefore(TODAY_AT_3_PM), is(equalTo(TODAY_AT_2_PM)));
+
+
+    }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -9,8 +9,11 @@ public class WhenWeCreateANewDog {
 
     @Test
     public void a_new_dog_should_have_a_name_and_a_breed_and_a_colour() throws Exception {
+
+        //When
         Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black");
 
+        //Then
         Assert.assertEquals("Fido",fido.getName());
         Assert.assertEquals("Labrador", fido.getBreed());
         org.assertj.core.api.Assertions.assertThat(fido.getColours()).containsExactly("Black");
