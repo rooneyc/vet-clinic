5a1c7420d8101ea972bd3013021868710984afaf
rooneyc <conor.p.rooney@aib.ie>
2017-05-12 16:14:53 +0100

    refactor

--- src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
@@ -1,21 +1,20 @@
 package serenitylabs.tutorials.vetclinic.collections.katas;
 
 import serenitylabs.tutorials.vetclinic.Pet;
-import serenitylabs.tutorials.vetclinic.collections.katas.BookingResponse;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class PetHotel {
+class PetHotel {
 
     private Map<String, Pet> pets = new TreeMap<>();
 
-    public Collection<Pet> getPets() {
+    Collection<Pet> getPets() {
         return pets.values();
     }
 
-    public BookingResponse checkIn(Pet pet) {
+    BookingResponse checkIn(Pet pet) {
         pets.put(pet.getName(), pet);
         return new BookingResponse(pet);
     }
