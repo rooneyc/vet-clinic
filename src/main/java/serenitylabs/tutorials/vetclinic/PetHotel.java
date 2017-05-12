ac42436c0bfa601fbadca93f043d38fe449f46dd
rooneyc <conor.p.rooney@aib.ie>
2017-05-12 15:35:03 +0100

    the_hotel_should_initially_have_no_pets_booked() passing

--- src/main/java/serenitylabs/tutorials/vetclinic/PetHotel.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/PetHotel.java
@@ -1,11 +1,12 @@
 package serenitylabs.tutorials.vetclinic;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PetHotel {
 
     public Collection<Pet> getPets() {
-        return null;
+        return new ArrayList<>();
     }
 
 }
