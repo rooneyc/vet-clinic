1955057e600c1a030b4bbdc02a0db3d57cb5dd7b
rooneyc <conor.p.rooney@aib.ie>
2017-05-13 11:30:44 +0100

    getPets(0

--- src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
@@ -1,4 +1,12 @@
 package serenitylabs.tutorials.vetclinic.collections.katas;
 
+import serenitylabs.tutorials.vetclinic.Pet;
+
+import java.util.Collection;
+
 class PetHotel {
+
+    public Collection<Pet> getPets() {
+        return null;
+    }
 }
