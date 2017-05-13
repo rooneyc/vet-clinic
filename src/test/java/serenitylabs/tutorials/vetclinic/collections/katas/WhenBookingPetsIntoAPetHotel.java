57c08e0cbb249333056f47cc454d657b0843535f
rooneyc <conor.p.rooney@aib.ie>
2017-05-13 11:24:51 +0100

    Reorder Tests to Match Read Me and Start 1 Hr Countdown

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/katas/WhenBookingPetsIntoAPetHotel.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/katas/WhenBookingPetsIntoAPetHotel.java
@@ -4,9 +4,8 @@ import org.junit.Test;
 
 public class WhenBookingPetsIntoAPetHotel {
 
-
     @Test
-    public void the_hotel_should_initially_have_no_pets_booked() {
+    public void the_hotel_should_initially_have_no_pets_booked() throws Exception {
     }
 
     @Test
@@ -22,11 +21,11 @@ public class WhenBookingPetsIntoAPetHotel {
     }
 
     @Test
-    public void should_be_able_to_retrieve_checked_in_pets_in_alphabetical_order() throws Exception {
+    public void should_be_able_to_obtain_a_booking_confirmation_when_we_check_in_a_pet() throws Exception {
     }
 
     @Test
-    public void should_be_able_to_obtain_a_booking_confirmation_when_we_check_in_a_pet() throws Exception {
+    public void should_be_able_to_retrieve_checked_in_pets_in_alphabetical_order() throws Exception {
     }
 
     @Test
@@ -37,7 +36,6 @@ public class WhenBookingPetsIntoAPetHotel {
     public void should_notify_owner_that_the_hotel_is_full() throws Exception {
     }
 
-
     @Test
     public void should_place_pets_on_a_waiting_list_when_the_hotel_is_full() throws Exception {
     }
@@ -45,7 +43,6 @@ public class WhenBookingPetsIntoAPetHotel {
     @Test
     public void pets_on_the_waiting_list_should_be_added_to_the_hotel_when_a_place_is_freed() throws Exception {
     }
-
 
     @Test
     public void pets_on_the_waiting_list_should_be_admitted_on_a_first_come_first_served_basis() throws Exception {
