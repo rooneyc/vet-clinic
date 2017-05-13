98aacf60d5ea60fe2e65b6a6818a8049013cd9a8
rooneyc <conor.p.rooney@aib.ie>
2017-05-13 12:13:44 +0100

    BookingAcknowledgement isConfirmed false

--- src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/BookingAcknowledgement.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/BookingAcknowledgement.java
@@ -1,4 +1,15 @@
 package serenitylabs.tutorials.vetclinic.collections.katas;
 
+import serenitylabs.tutorials.vetclinic.Pet;
+
 class BookingAcknowledgement extends BookingResponse {
+
+    public BookingAcknowledgement(Pet pet) {
+        super(pet);
+    }
+
+    boolean isConfirmed() {
+        return false;
+    }
+
 }
